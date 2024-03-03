4bdb8c846323e68d6c81414bf475c756ac4f4342
xint0n9.l <160615573+xint0n9@users.noreply.github.com>
2024-03-03 05:37:23 +0000

    final submission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 num = input('Enter a number (decimal only): ')
-# type your code here
-
-
-
+num = num.strip()
+dot = num.find(".")
+dp = len(num)-dot-1
 # do not change any code beyond this point
 print('The number', num, 'has', dp, 'decimal places.')
